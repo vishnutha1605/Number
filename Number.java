6e1061d56167a98c58df9a0470e005b6081f4829
vishnutha1605 <vishnuthamyakala07@gmail.com>
2025-01-29 05:45:45 -0800

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,12 @@
+public class Number {
+    public static void main(String[] args) {
+        int number = -3;
+        if (number > 0) {
+            System.out.println(number + "is positive");
+        } else if (number < 0) {
+            System.out.println(number + "is negative");
+        } else {
+            System.out.println("number is zero");
+        }
+    }
+}
